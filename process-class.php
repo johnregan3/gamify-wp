000c3d79878f0b8ef81fb3d1e94c81f689337107
johnregan3 <john.m.regan@gmail.com>
2013-07-29 03:50:24 -0500

    Removed Form from log settings page

--- process-class.php
+++ process-class.php
@@ -77,7 +77,7 @@ Class GAMWP_Process {
 				$add_to_array[$time]['activity_title'] = $activity_title;
 				$add_to_array[$time]['activity_points'] = $activity_points;
 
-				//Save activity array to two places:  Master Log, for tracking sitewide stats, and user_meta for user-focused stats.
+				//Save activity array to two places:  master_log, for tracking sitewide stats, and user_meta for focused stats.
 				//This saves looping through all actions ever just to get a single user's stats.
 
 				$master_log_array = get_option( 'gamwp_master_log' );
@@ -89,7 +89,6 @@ Class GAMWP_Process {
 				$user_meta_array = !empty( $user_meta_array ) ? $user_meta_array : array();
 				$new_user_meta_array = $user_meta_array + $add_to_array;
 				update_user_meta( $user_id, 'gamwp_user_log', $new_user_meta_array );
-
 
 				if( false === $updated_option ) {
 					$action_result['actions']['type'] = 'error';
--- settings/gamify-log.php
+++ settings/gamify-log.php
@@ -28,9 +28,6 @@ function generate_log_fields() {
 	foreach ( $options as $activity_time => $field_array ) {
 		ob_start();
 
-		$settings_value =( isset( $options[$activity_time]['delete'] ) ? $options[$activity_time]['delete'] : 'unchecked' );
-		echo "<td style='width:5%' ><input type='checkbox' id='gamwp_master_log[" . $activity_time . "][delete]' name='gamwp_master_log[" . $activity_time . "][delete]' value='checked' " . checked( 'checked', isset( $settings_value ) ? $settings_value : 'unchecked', false ) . " /></td>";
-
 		echo "<td>" . gmdate("M d Y H:i:s", $activity_time ) . " GMT</td>";
 
 		$settings_value =( isset( $options[$activity_time]['userid'] ) ? $options[$activity_time]['userid'] : '' );
@@ -60,34 +57,6 @@ function generate_log_fields() {
 function gamwp_points_log() {
 	?>
 
-
-<!-- Need to put this in a separate file -->
-
-<script>
-	function displayResult() {
-	var settingsValue = '';
-	var date = new Date();
-	date.toUTCString();
-	var newDate = Math.floor(date.getTime()/ 1000)
-
-	//newDate NEEDS TO = CURRENT TIME
-
-	var table=document.getElementById("gamwp-log-table");
-	var row=table.insertRow(1);
-	var cell1=row.insertCell(0);
-	var cell2=row.insertCell(1);
-	var cell3=row.insertCell(2);
-	var cell4=row.insertCell(3);
-	var cell5=row.insertCell(4);
-	cell1.innerHTML="<td><input type='checkbox' id='gamwp_master_log[" + newDate + "][delete]' name='gamwp_master_log[" + newDate + "][delete]' value='checked' /></td>";
-	cell2.innerHTML="<td><input type='hidden' id='gamwp_master_log[" + newDate + "]' name='gamwp_master_log[" + newDate + "]' value='checked' />Now</td>";
-	cell3.innerHTML="<td><input type='text' id='gamwp_master_log[" + newDate + "][userid]' name='gamwp_master_log[" + newDate + "][userid]' value='"+ settingsValue + "' title='"+ settingsValue + "' placeholder='Username' /></td>";
-	cell4.innerHTML="<td><input type='text' id='gamwp_master_log[" + newDate + "][activity_title]' name='gamwp_master_log[" + newDate + "][activity_title]' value='"+ settingsValue + "' title='"+ settingsValue + "' placeholder='Activity Title' /></td>";
-	cell5.innerHTML="<td><input type='text' id='gamwp_master_log[" + newDate + "][activity_points]' name='gamwp_master_log[" + newDate + "][activity_points]' value='"+ settingsValue + "' title='"+ settingsValue + "' placeholder='Points' /></td>";
-	}
-	</script>
-
-
 	<div id="gamwp-settings-wrap" class="wrap">
 		<div class="icon32" id="icon-options-general">
 			<br />
@@ -99,16 +68,9 @@ function gamwp_points_log() {
 			</div>
 		<?php } ?>
 		<p><a href="#">WIKI LINK</a>.</p>
-			<form method="post" action="options.php" enctype="multipart/form-data">
-				<?php settings_fields('gamwp_master_log_group'); ?>
-			<div class="tablenav top">
-				<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e( 'Save Changes', 'gamwp' ); ?>" />
-				<input type="button" class="button" onclick="displayResult()" value="<?php esc_attr_e( 'Add New Activity', 'gamwp' ); ?>">
-			</div>
 			<table class="wp-list-table widefat fixed posts" id="gamwp-log-table">
 				<thead>
 					<tr>
-						<th style='width:5%'><?php _e('Delete', 'gamwp'); ?></th>
 						<th><?php _e('Time', 'gamwp'); ?></th>
 						<th><?php _e('User', 'gamwp'); ?></th>
 						<th><?php _e('Activity', 'gamwp'); ?></th>
@@ -117,7 +79,6 @@ function gamwp_points_log() {
 				</thead>
 				<tfoot>
 					<tr>
-						<th style='width:5%'><?php _e('Delete', 'gamwp'); ?></th>
 						<th><?php _e('Time', 'gamwp'); ?></th>
 						<th><?php _e('User', 'gamwp'); ?></th>
 						<th><?php _e('Activity', 'gamwp'); ?></th>
@@ -134,11 +95,6 @@ function gamwp_points_log() {
 						<?php } ?>
 				</tbody>
 			</table>
-			<div class="tablenav bottom">
-				<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e( 'Save Changes', 'gamwp' ); ?>" />
-				<input type="button" class="button" onclick="displayResult()" value="<?php esc_attr_e( 'Add New Activity', 'gamwp' ); ?>">
-			</div>
-		</form>
 	</div>
 
 <?php
